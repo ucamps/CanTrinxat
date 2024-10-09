a84024b261cd3e2b2239a667042b0914440a6686
ucamps <ucamps12@gmail.com>
2024-10-09 19:47:08 +0200

    menu

--- js/global.js
+++ js/global.js
@@ -24,13 +24,26 @@ theToggle.onclick = function() {
 // funcion para que movil aparece el menu o se oculta
 theToggle.onclick = function() {
     toggleClass(this, 'on');
+    var article = document.getElementById('principal');
     var menu = document.getElementById('menu');
+    var footer = document.getElementById('foot');
+    var body = document.getElementById('body');
     if (hasClass(this, 'on')) {
       menu.style.opacity = 1;
       menu.style.visibility = 'visible';
+      article.style.visibility='hidden';
+      article.style.height='0';
+      menu.style.height = '75%';
+      footer.style.marginTop='125%';
+     body.style.overflow='hidden';
+
     } else {
       menu.style.opacity = 0;
       menu.style.visibility = 'hidden';
+      article.style.visibility='visible';
+      article.style.height='80%';
+      footer.style.marginTop='0%';
+      body.style.overflow='scroll';
     }
     return false;
   }
