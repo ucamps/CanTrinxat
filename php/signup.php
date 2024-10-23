5c2d4e3f947b02e2ad03b389d86af7dbaab3a9fd
ucamps <ucamps12@gmail.com>
2024-10-23 18:44:21 +0200

    prueba php

--- php/signup.php
+++ php/signup.php
@@ -9,22 +9,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($password !== $confirm_password) {
         echo "Las contraseñas no coinciden.";
         exit;
+    }else{
+        echo "hola";
     }
 
     // Hashear la contraseña
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    // $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     // Aquí deberías agregar la lógica para guardar el usuario en la base de datos
     // Asegúrate de hashear la contraseña
 
     // Establecer la cookie si el usuario quiere ser recordado (opcional)
-    if (isset($_POST['remember'])) {
-        setcookie('email', $email, time() + (86400 * 30), "/", "", true, true); // Cookie válida por 30 días
-    }
+    // if (isset($_POST['remember'])) {
+    //     setcookie('email', $email, time() + (86400 * 30), "/", "", true, true); // Cookie válida por 30 días
+    // }
 
     // Redirigir a la página de inicio
-    header("Location: index.html");
-    exit;
+    // header("Location: index.html");
+    // exit;
 }
  else {
     // Código para manejar solicitudes que no sean POST
